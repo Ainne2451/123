b007edf525cbfbb69f7c6842277ae15dbe341c92
Ainne2451 <62133524+Ainne2451@users.noreply.github.com>
2020-06-18 23:38:18 +0800

    Update views.py

--- students/views.py
+++ students/views.py
@@ -25,11 +25,12 @@ parser  = WebhookParser(settings.LINE_CHANNEL_SECRET)
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('W3cJZwxPee8Xg74vRZ0KvQ7Sy0sjPpzlCnuhE59pXLUJrBUghurpxP7K9uqEK/cXYZyIgD6B8J926PB2Bfc73pT1tscfORX8koAxTWJZesgAF9LdzgKrlRnURH2m32Mm96s81v//uGWYdEE7cfZ2uwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('CxVO4fNcPJFe3hszmu//G4BnLFRyhwv2pByFcB6h48HeelcuweOrvwg2+VHahoocWQyKqMyFI+Px27xdcUTLSxI8qPNz9v2+16nC26iUgqvbjcj6Mbwe+tJ5sgcv4EvLNNg8vL7VYIF2zIUoA0/YHgdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('02e59494ae3f01a49a87f538ff81ffb0')
+handler = WebhookHandler('339f6e2054c10bc8f4fd19e2277fd4c8')
 
-line_bot_api.push_message('U7638b973e5e09749f86ecf44256f64d4', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('Uae6995ae4c73190deaa1d36b1c3f1a7b', TextSendMessage(text='你可以開始了'))
+
 
 @csrf_exempt
 @require_POST
